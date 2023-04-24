9f47c5bfc7f1e72b494e5ef1f4cc7899e33a62c0
ZamarShoo <shg.ivan97@gmail.com>
2023-04-24 18:41:43 +0300

    we a help

--- src/assets/scripts/sliders.js
+++ src/assets/scripts/sliders.js
@@ -28,7 +28,22 @@ const need_help_index = new Swiper(".adress-help_body", {
     nextEl: ".adress-help-next",
     prevEl: ".adress-help-prev",
   },
-  // pagination: {
-  //   el: ".first-slide_swiper-pagination",
-  // },
+});
+
+const we_help_index = new Swiper(".we-help_body", {
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    650: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".we-help-next",
+    prevEl: ".we-help-prev",
+  },
 });
